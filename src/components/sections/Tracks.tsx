cbc14deb484c872c63cce3d352a0f7acd740e480
PixieDust. <aryaravapradana5555@gmail.com>
2026-04-21 23:03:03 +0700

    updatez

--- src/components/sections/Tracks.tsx
+++ src/components/sections/Tracks.tsx
@@ -20,7 +20,7 @@ const tracks = [
         color: 'from-neon-purple to-purple-600',
         accentHex: '#A856EE',
         href: '/kompetisi/ui-ux',
-        tags: ['SMA/SMK', 'Mahasiswa', 'Umum', 'Early Bird Extended!'],
+        tags: ['SMA/SMK', 'Mahasiswa', 'Umum'],
     },
     {
         icon: BusinessCaseIcon,
@@ -29,7 +29,7 @@ const tracks = [
         color: 'from-neon-orange to-orange-600',
         accentHex: '#FF8B53',
         href: '/kompetisi/business-case',
-        tags: ['Mahasiswa', 'Umum', 'Early Bird Extended!'],
+        tags: ['Mahasiswa', 'Umum'],
     },
     {
         icon: WebDevIcon,
@@ -38,7 +38,7 @@ const tracks = [
         color: 'from-neon-blue to-blue-600',
         accentHex: '#1DBCD3',
         href: '/kompetisi/web-dev',
-        tags: ['SMA/SMK', 'Mahasiswa', 'Umum', 'Early Bird Extended!'],
+        tags: ['SMA/SMK', 'Mahasiswa', 'Umum'],
     }
 ];
 
